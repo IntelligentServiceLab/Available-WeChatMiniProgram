c766dc0e7fa40e2e98bde927574c45ce27d68469
Nick <2042772082@qq.com>
2024-01-10 16:10:56 +0800

    2024/1/10

--- frontend_code/cloudgosystem/config.js
+++
@@ -1,2 +0,0 @@
-const API_PREFIX = 'http://118.25.40.159:8004';
-export default API_PREFIX;
--- frontend_code/cloudgosystem/libs/config.js
+++
@@ -1,4 +0,0 @@
-var config = {
-	key : "153c1c41c78c0000d77c739583592a24"
-}
-module.exports.config = Config
